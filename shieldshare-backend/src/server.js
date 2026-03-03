c344bb28a9117e821620e2d7b1e82471e656dbba
Chaitanya Shinde <Chaitanyashinde8290@gmail.com>
2026-03-03 15:02:14 +0530

    [FEAT]: complete backend MVP with MongoDB, auth, and secure document sharing

--- shieldshare-backend/src/server.js
+++ shieldshare-backend/src/server.js
@@ -1,9 +1,12 @@
 const app = require("./app.js");
 const dotenv = require("dotenv");
+const connectDB = require("./config/db.js");
 
 dotenv.config();
 
 const PORT = process.env.PORT;
+
+connectDB();
 
 app.listen(PORT, () => {
   console.log(`🚀 ShieldShare server running on port ${PORT}`);
